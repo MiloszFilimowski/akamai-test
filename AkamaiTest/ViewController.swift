10179e9706071620033b6cfe77ec0151b4a31639
Miłosz Filimowski <milosz.filimowski@flipfit.com>
2021-04-19 13:26:45 +0200

    Add example

--- AkamaiTest/ViewController.swift
+++ AkamaiTest/ViewController.swift
@@ -1,19 +1,14 @@
-//
-//  ViewController.swift
-//  AkamaiTest
-//
-//  Created by Miłosz Filimowski on 15/04/2021.
-//
-
 import UIKit
-
+import AmpCore
 class ViewController: UIViewController {
-
+    
+    var ampPlayer: AmpPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        ampPlayer = AmpPlayer(parentView: view)
+        ampPlayer.setLicense("THE_KEY")
+        ampPlayer.play(url: "https://multiplatform-f.akamaihd.net/i/multi/will/bunny/big_buck_bunny_,640x360_400,640x360_700,640x360_1000,950x540_1500,.f4v.csmil/master.m3u8")
+        ampPlayer.autoplay = true
     }
-
-
 }
-
---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'AkamaiTest' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for AkamaiTest
+
+  pod 'AmpCore', '9.1.1'
+
+
+end
